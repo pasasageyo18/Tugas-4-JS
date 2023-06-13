6ec52dbfa00b999e47d53276816406a5c25a2367
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-13 16:40:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function tinggiBadan() {
+  let joni = 170;
+  let budi = 160;
+  let naufal = 140;
+
+  if (joni > budi && joni > naufal) {
+    console.log("Joni adalah siswa paling tinggi");
+    console.log(joni, budi, naufal);
+  } else {
+    console.log("tinggi mereka sama");
+  }
+}
+
+tinggiBadan();
